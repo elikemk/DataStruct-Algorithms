4fe32e730832b81153630b783df9b582cc024c75
Elikem Kalitsi <101428026+elikemk@users.noreply.github.com>
2024-02-01 10:22:54 +0000

    Update hw3.cpp

--- hw3.cpp
+++ hw3.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int precedence (char c){
+        if ( c == '^')
+        return 3;
+        else if (c == '/' || c == '*')
+        return 2;
+        else if (c == '+' || c == '-')
+        return 1;
+        else
+        return -1;
+    }
+    char a (char c){
+        if (c == '^')
+        return 'R';
+        return 'L';
+    }
+
+    
+    main()
+    a = "ABC +-"
+    b = "ABC-D*+"
+    c = "AB+C-DE*+"
+    inflix(a)
+    inflix(b)
+    inflix(c)
+}
