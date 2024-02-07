dc770c4b9fb9d44e81d7b7a2054b451ac0f3b26f
Elikem Kalitsi <101428026+elikemk@users.noreply.github.com>
2024-02-07 04:20:16 +0000

    Update binarysearchtree.cpp

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -1,65 +1,85 @@
-#include <iostream>
-using namespace std; 
+
+#include <bits/stdc++.h>
+using namespace std;
+ //Elikem Kalitsi Feb 6th 
+//create the tree
 
 struct node
 {
     int key;
     struct node *left, *right;
-}
-//create the tree
-stuct node* newNode(int item)
-{
-  struct node* temp = (struct node*) malloc(
-  sizeof (struct));
-  temp -> key = item; 
-  temp -> left = temp -> right = NULL;
-  return temp;  
-}
-
+};
+ 
 //insert the node intert a new node
-struct *node insert(struct node* node, int key){
-    //if the tree is empty why return a new node?
-    if (node == NULL)
-    return newNode (key); 
-
-    //what is recuring down the tree mean?
-    if (key < node -> key)
-    {
-        node -> left = insert(node-> left , key);
-    }
-    else if (key > node ->)
-    {
-        node -> right = insert(node -> right , key);
-    }
-    
-    //return the node pointer
-    return node; 
-        
+struct node* newNode(int item)
+{
+    struct node* temp = (struct node*)malloc(
+            sizeof(struct node));
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
 }
-// How to traversal of Binary Search Tree
-void inorder(struct node* root){
-    if (root != NULL){
-        inorder(root -> left);
-        cout << " " << root -> key;
-        inorder(root -> right);
-    }
+ 
 
-    int main(){
-        struct node* root = NULL;
-        root = insert(root, 12);
-        insert(root, 22);
-        insert(root, 83); 
-        insert(root, 10);
-        insert(root, 88);
-        insert(root, 290);
-        insert(root, 39);
-        insert(root, 540);
-        insert(root, 5993);
-        insert(root, 2949);
-        insert(root, 399);
-        
-            
+struct node* insert(struct node* node, int key)
+{
+     
+    if (node == NULL)
+        return newNode(key);
+ 
+    if (key < node->key) 
+    {
+        node->left = insert(node->left, key);
+    }
+    else if (key > node->key)
+    {
+        node->right = insert(node->right, key);
     }
  
+    // Return the node pointer
+    return node;
 }
+ 
+// Function BST
+void inorder(struct node* root)
+{
+    if (root != NULL) 
+    {
+        inorder(root->left);
+        cout << " " << root->key;
+        inorder(root->right);
+    }
+}
+ 
+int main()
+{
+     
+   
+    struct node* root = NULL;
+ 
+ 
+    root = insert(root, 11);
+    insert(root, 290);
+    insert(root, 2000);
+    insert(root, 4000);
+    insert(root, 720);
+    insert(root, 6390);
+    insert(root, 390);
+    insert(root, 8679);
+    insert(root, 7890);
+    insert(root, 2334);
+    insert(root, 4567);
+    insert(root, 465);
+    insert(root, 5);
 
+
+
+
+
+ 
+    // Function Call
+    inorder(root);
+ 
+    return 0;
+}
+ 
