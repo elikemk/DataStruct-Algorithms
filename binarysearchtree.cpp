9c0d9b3ec2e1c9f235c014b6c3a3f93dfab68749
Elikem Kalitsi <101428026+elikemk@users.noreply.github.com>
2024-02-07 04:04:18 +0000

    Update binarysearchtree.cpp

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -21,4 +21,45 @@ struct *node insert(struct node* node, int key){
     //if the tree is empty why return a new node?
     if (node == NULL)
     return newNode (key); 
+
+    //what is recuring down the tree mean?
+    if (key < node -> key)
+    {
+        node -> left = insert(node-> left , key);
+    }
+    else if (key > node ->)
+    {
+        node -> right = insert(node -> right , key);
+    }
+    
+    //return the node pointer
+    return node; 
+        
 }
+// How to traversal of Binary Search Tree
+void inorder(struct node* root){
+    if (root != NULL){
+        inorder(root -> left);
+        cout << " " << root -> key;
+        inorder(root -> right);
+    }
+
+    int main(){
+        struct node* root = NULL;
+        root = insert(root, 12);
+        insert(root, 22);
+        insert(root, 83); 
+        insert(root, 10);
+        insert(root, 88);
+        insert(root, 290);
+        insert(root, 39);
+        insert(root, 540);
+        insert(root, 5993);
+        insert(root, 2949);
+        insert(root, 399);
+        
+            
+    }
+ 
+}
+
